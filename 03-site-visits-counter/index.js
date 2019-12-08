baff90c9104fb014f8d981e55129deee89ddcac5
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-12-08 16:10:56 -0600

    connecting the visits app to the redis container

--- 03-site-visits-counter/index.js
+++ 03-site-visits-counter/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+
+// we use the redis container name as its connection url
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 
 client.set('visits', 0);
 
